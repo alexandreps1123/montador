e2409d722ddded5cbd10dc55b5184316f5c45f53
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-05-07 01:47:59 -0300

    feat: validating input args

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
+#include "messages.h"
+#include "handle_input.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    printf("Hello World\n");
+int main(int argc, char *argv[])
+{
+    checkArgs(argc, argv);
+
+    printf("Passou\n");
+
     return 0;
+}
+
+void readFile(char flag[], char fileName[])
+{
+    // char str[] = fileName;
+    char *pch;
+    pch = strtok(fileName, ".");
+    while (pch != NULL)
+    {
+        printf("%s\n", pch);
+        pch = strtok(NULL, ".");
+    }
+    
 }
