d21d29e8716bc5d584abb9eb321cdcaf45b8881f
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-05-18 23:48:23 -0300

    feat: some example input files

---
+++ exemplo_soma_dois_numeros.asm
@@ -0,0 +1,11 @@
+input n1;le o primeiro numero
+input n2;le o segundo numero
+load n1;carega n1 no acumulador
+add n2;soma n2 com o conteudo do acumulador
+store n3;armazena a sima n1+n2 em n3
+output n3;escreve o resultado
+stop;termina a execucao do programa
+n1: space;reserva espaco
+n2: space;reserva espaco
+n3: space;reserva espaco
+
