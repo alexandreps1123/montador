f18f6a708c063d64e6f2073fb8d0dcd2d6a6406d
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-05-26 16:12:01 -0300

    feat: directive EQU (alias) and IF, IF 1 show next line, IF 0 dont show

--- pre_processor.c
+++ pre_processor.c
@@ -1,61 +1,124 @@
 #include "pre_processor.h"
 
 const int MAX_LINE_LENGTH = 256;
+const int MAX_TOKEN_LENGTH = 80;
+const int MAX_EQU_DECLARATION = 20;
 
-void preProcessor(FILE *pFile) 
+void preProcessor(FILE *pFile)
 {
     char lineContent[MAX_LINE_LENGTH];
     char line[MAX_LINE_LENGTH];
+
     int countPureLabel = 0;
 
-    clearString(lineContent);
-    clearString(line);
+    int ifAction = 1;
 
-    while (fgets(lineContent , MAX_LINE_LENGTH, pFile) != NULL) 
+    int countEqu = 0;
+    EQU equ[MAX_EQU_DECLARATION];
+
+    // Queue q;
+
+    // createQueue(&q);
+
+    clearString(lineContent, MAX_LINE_LENGTH);
+    clearString(line, MAX_LINE_LENGTH);
+
+    while (fgets(lineContent, MAX_LINE_LENGTH, pFile) != NULL)
     {
         removeComments(lineContent);
         removeUnnecessaryCharacters(lineContent);
         toUpperCase(lineContent);
 
         // TODO: verify if is a label
-        if (validInitialCharacter(lineContent)) {
-            if(isLabel(lineContent) == 1 && isPureLabel(lineContent) == 1) 
+        if (validInitialCharacter(lineContent))
+        {
+            if (isLabel(lineContent) == 1 && isPureLabel(lineContent) == 1)
             {
                 countPureLabel++;
                 strcat(line, lineContent);
             }
-            else 
+            else
             {
-                if(countPureLabel != 0) 
+                for (int i = 0; i < countEqu; i++)
+                {
+                    changeForEQUValue(lineContent, equ[i]);
+                }
+                
+                if (countPureLabel != 0)
                 {
                     countPureLabel = 0;
                     strcat(line, lineContent);
                     strcpy(lineContent, line);
-                    clearString(line);
+                    clearString(line, MAX_LINE_LENGTH);
                 }
 
-                printf("%s\n", lineContent);
+                if (isEQU(lineContent))
+                {
+                    int countToken = 0;
+                    char *pch;
+
+                    pch = strtok(lineContent, ":");
+
+                    strcpy(equ[countEqu].label, pch);
+
+                    while (pch != NULL)
+                    {
+                        if (countToken == 2)
+                            strcpy(equ[countEqu].value, pch);
+
+                        pch = strtok(NULL, " ");
+                        countToken++;
+                    }
+
+                    countEqu++;
+
+                    clearString(lineContent, MAX_LINE_LENGTH);
+
+                    continue;
+                }
+
+                if (isIF(lineContent)) {
+                    ifAction = getIfValue(lineContent);
+                    continue;
+                }
+
+                if(ifAction == 0) {
+                    ifAction = 1;
+                    continue;
+                }
+                else printf("%s\n", lineContent);
             }
         }
 
-        clearString(lineContent);
+        clearString(lineContent, MAX_LINE_LENGTH);
     }
+
+    // for (size_t i = 0; i < countEqu; i++)
+    // {
+    //     // equ = *(printHead(&q));
+    //     printf("Numero: %s\n", equ[i].label);
+    //     printf("Numero: %s\n", equ[i].value);
+    //     // dequeue(&q);
+    //     // countEqu--;
+    // }
+
     // TODO: write in a file pre processed data
-    
+    // clear(&q);
+
     return;
 }
 
-void clearString(char lineContent[MAX_LINE_LENGTH]) {
+void clearString(char *lineContent, int length)
+{
     int i = 0;
-    while (i < MAX_LINE_LENGTH)
+    while (i < length)
     {
         lineContent[i] = '\0';
         i++;
     }
-    
 }
 
-void removeComments(char *lineContent) 
+void removeComments(char *lineContent)
 {
     strtok(lineContent, ";");
     return;
@@ -63,8 +126,7 @@ void removeComments(char *lineContent)
 
 void removeUnnecessaryCharacters(char *lineContent)
 {
-    char *formatedLine = malloc(strlen(lineContent)); 
-    char *pch, *temp = lineContent;
+    char *pch, *formatedLine = malloc(strlen(lineContent));
 
     // ' '	(0x20)	space (SPC)
     // '\t'	(0x09)	horizontal tab (TAB)
@@ -72,7 +134,7 @@ void removeUnnecessaryCharacters(char *lineContent)
     // '\v'	(0x0b)	vertical tab (VT)
     // '\f'	(0x0c)	feed (FF)
     // '\r'	(0x0d)	carriage return (CR)
-    pch = strtok(temp, " \t\n\r\v\f\r");
+    pch = strtok(lineContent, " \t\n\r\v\f\r");
     while (pch != NULL)
     {
         strcat(formatedLine, pch);
@@ -80,14 +142,15 @@ void removeUnnecessaryCharacters(char *lineContent)
         pch = strtok(NULL, " \t\n\r\v\f\r");
     }
 
+    clearString(lineContent, MAX_LINE_LENGTH);
     strcpy(lineContent, formatedLine);
 
     return;
 }
 
-void toUpperCase(char *lineContent) 
+void toUpperCase(char *lineContent)
 {
-    int i=0;
+    int i = 0;
     while (i < strlen(lineContent))
     {
         lineContent[i] = toupper(lineContent[i]);
@@ -97,17 +160,18 @@ void toUpperCase(char *lineContent)
     return;
 }
 
-int validInitialCharacter(char *lineContent) 
+int validInitialCharacter(char *lineContent)
 {
     return !(lineContent[0] == '\n' || lineContent[0] == ';' || lineContent[0] == NULL);
 }
 
-int isLabel(char *lineContent) 
+int isLabel(char *lineContent)
 {
     int i = 0;
     while (i < MAX_LINE_LENGTH)
     {
         if (lineContent[i] == ':') return 1;
+
         i++;
     }
 
@@ -117,19 +181,12 @@ int isLabel(char *lineContent)
 int isPureLabel(char *lineContent)
 {
     int countToken = 0;
-    char *formatedLine = malloc(strlen(lineContent)); 
-    char *pch;
+    char *pch, *formatedLine = malloc(strlen(lineContent));
 
-    // ' '	(0x20)	space (SPC)
-    // '\t'	(0x09)	horizontal tab (TAB)
-    // '\n'	(0x0a)	newline (LF)
-    // '\v'	(0x0b)	vertical tab (VT)
-    // '\f'	(0x0c)	feed (FF)
-    // '\r'	(0x0d)	carriage return (CR)
     pch = strtok(lineContent, ":");
     while (pch != NULL)
     {
-        if(countToken == 0) 
+        if (countToken == 0)
         {
             strcat(formatedLine, pch);
             strcat(formatedLine, ": ");
@@ -143,8 +200,48 @@ int isPureLabel(char *lineContent)
         countToken++;
     }
 
-    clearString(lineContent);
+    clearString(lineContent, MAX_LINE_LENGTH);
     strcpy(lineContent, formatedLine);
 
     return countToken;
+}
+
+int isEQU(char *lineContent)
+{
+    char *pch;
+    pch = strstr(lineContent, "EQU");
+
+    if (pch != NULL) return 1;
+
+    return 0;
+}
+
+void changeForEQUValue(char *lineContent, EQU equ)
+{
+    char *pch;
+
+    pch = strstr(lineContent, equ.label);
+    if (pch != NULL) strncpy(pch, equ.value, MAX_TOKEN_LENGTH);
+    return;
+}
+
+int isIF(char *lineContent)
+{
+    char *pch;
+    pch = strstr(lineContent, "IF");
+
+    if (pch != NULL) return 1;
+
+    return 0;
+}
+
+int getIfValue(char *lineContent)
+{
+    char *pch, *formatedLine = malloc(strlen(lineContent));
+
+    pch = strtok(lineContent, " ");
+    pch = strtok(NULL, " \n");
+    strcat(formatedLine, pch);
+
+    return atoi(formatedLine);
 }
--- pre_processor.h
+++ pre_processor.h
@@ -6,13 +6,23 @@
 #include <string.h>
 #include <ctype.h>
 
+typedef struct EQU
+{
+    char label[80];
+    char value[80];
+} EQU;
+
 void preProcessor(FILE *pFile);
-void clearString(lineContent);
+void clearString(char *lineContent, int lenght);
 void removeComments(char *lineContent);
 void removeUnnecessaryCharacters(char *lineContent);
 void toUpperCase(char *lineContent);
 int validInitialCharacter(char *lineContent);
 int isLabel(char *lineContent);
 int isPureLabel(char *lineContent);
+int isEQU(char *lineContent);
+void changeForEQUValue(char *lineContent, EQU equ);
+int isIF(char *lineContent);
+int getIfValue(char *lineContent);
 
 #endif /* __PRE_PROCESSOR__ */
