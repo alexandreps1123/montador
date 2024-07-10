80126dfdfd6ff606731616e810a6b5c0ed396be6
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-07-10 02:56:40 -0300

    feat: manager file moved to assembler and pre_processor

--- main.c
+++ main.c
@@ -1,66 +1,19 @@
 #include "messages.h"
 #include "input_handler.h"
-#include "file_handler.h"
 #include "pre_processor.h"
 #include "assembler.h"
-// #include <stdio.h>
-// #include <string.h>
-// #include <stdlib.h>
-// #include <ctype.h>
-
-// Função para remover espaços em branco extras de uma linha
-// void trim(char *str) {
-//     int i = 0, j = -1;
-//     while (str[i]) {
-//         if (!isspace(str[i]) || (i > 0 && !isspace(str[i - 1]))) {
-//             str[++j] = str[i];
-//         }
-//         i++;
-//     }
-//     str[++j] = '\0';
-// }
-
-// void preProcessor(char *str){
-
-// }
-
-// void assembler(char *str){
-//     switch(){
-//         case 1:
-//             break;
-//         default:
-//             break;
-//     }
-// }
-
-
 
 int main(int argc, char *argv[])
 {
-    FILE *pFile;
-
     checkArgs(argc, argv);
 
     if(strcmp(argv[1], "-p")==0)
     {
-        Text text;
-
-        pFile = openFile(argv[2]);
-        text = preProcessor(pFile);
-
-        writeFile(argv[2], &text);
-
-        fclose(pFile);
+        preProcessor(argv[2]);
     } 
     else if(strcmp(argv[1], "-o")==0)
     {
-        pFile = openFile(argv[2]);
-
-        assembler(pFile);
-
-        // writeFile(argv[2]);
-
-        fclose(pFile);
+        assembler(argv[2]);
     }
 
     return 0;
