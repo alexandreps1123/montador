f5ebe7e22b18aefbbe069ce5fa731e9cc3e5b022
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-07-10 02:53:05 -0300

    feat: methods used in assembler and pre_processor

---
+++ common.c
@@ -0,0 +1,25 @@
+#include "pre_processor.h"
+
+void clearString(char *lineContent, int length)
+{
+    int i = 0;
+    while (i < length)
+    {
+        lineContent[i] = '\0';
+        i++;
+    }
+}
+
+int isLabel(char *lineContent)
+{
+    int i = 0;
+    while (i < MAX_LINE_LENGTH)
+    {
+        if (lineContent[i] == ':') return 1;
+
+        i++;
+    }
+
+    return 0;
+}
+
---
+++ common.h
@@ -0,0 +1,13 @@
+#ifndef __COMMON__
+#define __COMMON__
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void clearString(char *lineContent, int lenght);
+int isLabel(char *lineContent);
+
+
+#endif /* __COMMON__ */
